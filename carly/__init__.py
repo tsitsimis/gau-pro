6950806b15cee3878f14659c2de73d66e59eb58b
Theodore Tsitsimis <thodotsi@gmail.com>
2017-12-29 19:45:41 +0200

    updated __init__

--- carly/__init__.py
+++ carly/__init__.py
@@ -1,4 +1,4 @@
 from .regression import *
 from .utils import *
-
+from .kernels import *
 
--- tests/test.py
+++ tests/test.py
@@ -18,13 +18,13 @@ n_test = 200
 x_test = np.linspace(x_lim[0], x_lim[1], n_test)
 
 # fit
-ker = lambda x, y: np.exp(-0.5 * np.linalg.norm(x - y) ** 2 / 1.0)
-model = carly.Regression(ker, sigma_n=sigma_n)
+model = carly.Regression('se', sigma_n=sigma_n)
 model.fit(x_train, f_train, x_test)
 
 # pick samples
 samples = model.pick_samples(1)
 
+# plot
 t = np.linspace(x_lim[0], x_lim[1], 100)
 plt.plot(t, uu.objective(t), c='b')
 plt.scatter(x_train, f_train, marker='+', c='r', s=120, zorder=10)
