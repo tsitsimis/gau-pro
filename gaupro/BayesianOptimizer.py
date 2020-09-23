7ede97cfa47bc6726e6e954dc083ba6223c77cc0
Theodore Tsitsimis <th.tsitsimis@gmail.com>
2020-09-23 15:08:51 +0300

    Refactor

--- gaupro/BayesianOptimizer.py
+++ gaupro/BayesianOptimizer.py
@@ -8,20 +8,24 @@ class BayesianOptimizer:
         self.acquisition_func = acquisition_func
         self.policy = policy
 
-        self.__find_next_x()
-
         self.history = np.empty((2, 0))
-        # if the model is fitted already, add the train inputs to optimizer's history
-        model_history = np.concatenate((self.model.X, self.model.y.T))
-        self.history = np.concatenate((self.history, model_history), axis=1)
 
-        y_next = self.black_box(self.x_next)
-        self.history = np.append(self.history, np.array([[self.x_next], [y_next]]), axis=1)
+        if model.X_train is not None:
+            self.__find_next_x()
+            
+            model_history = np.concatenate((self.model.X_train, self.model.y_train.T))
+            self.history = np.concatenate((self.history, model_history), axis=1)
+            y_next = self.black_box(self.x_next)
+            self.history = np.append(self.history, np.array([[self.x_next], [y_next]]), axis=1)
 
-    def update(self):
+        else:
+            self.x_next = 0
+
+    def update(self, x_test):
         y_next = self.black_box(self.x_next)           # sample the black box function
         self.model.augment_train(self.x_next, y_next)  # augment the data
-        self.model.fit(self.model.X, self.model.y)     # update the GP
+        self.model.fit(self.model.X_train, self.model.y_train)     # update the GP
+        self.model.predict(x_test)
 
         self.history = np.append(self.history, np.array([[self.x_next], [y_next]]), axis=1)
         self.__find_next_x()  # find next x
